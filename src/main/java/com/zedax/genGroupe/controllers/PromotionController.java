81287c64edb8f59b41dade6c9dac1e40936e22ad
HadyFof <carterhedy57@gmaol.com>
2021-12-21 15:44:21 +0000

    Update

--- src/main/java/com/zedax/genGroupe/controllers/PromotionController.java
+++ src/main/java/com/zedax/genGroupe/controllers/PromotionController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/api")
 
-public class AdminController {
+public class PromotionController {
 
     @Autowired
     PromotionServiceImp promotionServiceImp;
